dfdc1fed0515ff17fe60032f94867de8d829e797
sherjaa <sherraa32@gmail.com>
2021-09-20 19:23:57 +0300

    first commit

---
+++ script.js
@@ -0,0 +1,14 @@
+var birinchi = parseFloat(prompt('1-son'))
+var ikkinchi = parseFloat(prompt('2-son'))
+var uchinchi = parseFloat(prompt('3-son'))
+var tortinchi = parseFloat(prompt('4-son'))
+var beshinchi = parseFloat(prompt('5-son'))
+var oltinchi = parseFloat(prompt('6-son'))
+
+var jami = beshinchi + ikkinchi + uchinchi + tortinchi + beshinchi + oltinchi
+
+console.log("yig\'indisi " + jami)
+
+var ortaArifmetik = jami / 6
+
+console.log(ortaArifmetik)
